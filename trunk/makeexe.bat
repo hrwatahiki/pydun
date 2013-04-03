b7dc80b61e3ef6ef6dfb67824f2833514468c981
hrwatahiki <hrwatahiki@44a3fe10-d6a8-4042-99bc-5bf971f0e03b>
2013-04-03 17:17:45 +0000

    メニューの表記を修正。 Pydunに統一。#31046

--- trunk/makeexe.bat
+++ trunk/makeexe.bat
@@ -1,3 +1,3 @@
-c:\python27\python.exe C:\python\pyinstaller-2.0\pyinstaller.py --onefile --windowed .\pydun.py
-copy .\dist\pydun.exe .\
+c:\python27\python.exe C:\python\pyinstaller-2.0\pyinstaller.py --onefile --windowed .\Pydun.py
+copy .\dist\Pydun.exe .\Pydun.exe
 pause
